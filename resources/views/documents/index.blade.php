3110afd87874851a3ce0d0b849d581a7cb95b591
Masahiro111 <33553834+Masahiro111@users.noreply.github.com>
2021-11-20 23:36:48 +0900

    fix typo

--- resources/views/documents/index.blade.php
+++ resources/views/documents/index.blade.php
@@ -47,7 +47,7 @@
                                             <a
                                                href="{{ route('documents.create') }}"
                                                class="inline-flex items-center px-4 py-2 bg-green-500 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring ring-gray-300 disabled:opacity-25 transition ease-in-out duration-150">
-                                                Upload
+                                                Download
                                             </a>
                                             <a
                                                href="{{ route('documents.create') }}"
